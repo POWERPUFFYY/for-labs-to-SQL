3a648181d0ce3a4f83ac94ae54b7e332626081e5
POWERPUFFY <pankin.pv@mail.ru>
2021-12-16 15:35:03 +0300

    Лаба 6

---
+++ data.sql
@@ -0,0 +1,16 @@
+insert into usr (first_name, middle_name, last_name, email) values  
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'), 
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'), 
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Бек', 'Юрьевич', 'Калашников', 'kalashnikov@my.example.com'),
+('Наталья', 'Николаевна', 'Поклонская', 'poklonskaya@my.example.com'),
+('Александр', 'Павлович', 'Ладыгин', 'ladygin@my.example.com'),
+('Павел', 'Александрович', 'Панкин', 'pankin@my.example.com'),
+('Юлия', 'Владимировна', 'Гладышева', 'gladysheva@my.example.com'),
+('Диана', 'Даниловна', 'Пушкина', 'pishkina@my.example.com'),
+('Андрей', 'Петрович', 'Бурыкин', 'burykin@my.example.com'),
+('Никита', 'Германович', 'Симонов', 'simonov@my.example.com'),
+('Дарья', 'Артемовна', 'Кондрашина', 'kondrashina@my.example.com'),
+('Даниил', 'Петрович', 'Ларионов', 'larionov@my.example.com');
+
+select * from usr;
